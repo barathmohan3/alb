c624b7e18d05070aaaebc82743484a7258f8afc7
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-05-30 23:57:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "my-unique-tf-state-bucket"
+    bucket         = "my-oidc-bucket-15328069840"
     key            = "alb-path-routing/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
